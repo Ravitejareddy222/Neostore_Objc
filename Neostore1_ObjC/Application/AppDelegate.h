e630e242b4d1c19b02b36c5b49a5393375a54a18
ravi <ravitejareddy222>
2024-12-06 15:33:37 +0530

    first commit

---
+++ Neostore1_ObjC/Application/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Neostore1_ObjC
+//
+//  Created by Neo on 06/12/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Neostore1_ObjC/Application/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Neostore1_ObjC
+//
+//  Created by Neo on 06/12/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
